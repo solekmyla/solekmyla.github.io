1227954de8effc954ccbceadaf17f0cc201ba10b
Lucas Lemos <LemosLucas@users.noreply.github.com>
2017-06-20 15:21:09 +0200

    add draging capabilty

--- gipsa/sketch.js
+++ gipsa/sketch.js
@@ -1,35 +1,56 @@
 
-var x = 0, y = 0, r, angle;
+var filename = 'img0013.jpg';
+var zoomLevel = 2;
+var imgWidth = 255 * zoomLevel;
+var imgHeight = 255 * zoomLevel;
+var squareSize = imgWidth/2;
 
 function preload() {
-  img = loadImage('assets/img0013.jpg');
+  img = loadImage('assets/' + filename);
 }
 
 function setup() {
-  createCanvas(900, 900);
-  zoomLevel = 3;
-  imgWidth = 255 * zoomLevel;
-  imgHeight = 255 * zoomLevel;
-  squareSize = imgWidth/2;
+  createCanvas(imgWidth, imgHeight);
+  background(51);
+  x_left = round(imgWidth/2) - round(squareSize/2);
+  y_top = round(imgHeight/2) - round(squareSize/2);
+}
 
+function draw() {
+  background(img);
 
-  image(img, 0, 0, imgWidth, imgHeight);
+  noFill();
+  stroke(255, 120, 0);
+  rect(x_left, y_top, round(squareSize), round(squareSize));
 
   stroke(255, 0, 0);
   line(round(imgWidth/2), 0, round(imgWidth/2), imgHeight);
   line(0, round(imgHeight/2), imgWidth, round(imgHeight/2));
 
-  noFill();
-  stroke(255, 120, 0)
-  x_topLeft = round(imgWidth/2) - squareSize/2;
-  y_topLeft = round(imgHeight/2) - squareSize/2;
-
-  rect(x_topLeft, y_topLeft, squareSize, squareSize);
+  if (mouseIsPressed) {
+    var x_right = x_left + round(squareSize);
+    var y_bottom = y_top + round(squareSize);
 
 
 
+
+    if (mouseX >= x_left && mouseX <= x_right) {
+      if (mouseY >= y_top && mouseY <= y_bottom) {
+        x_left = mouseX - round(squareSize/2);
+        y_top  = mouseY - round(squareSize/2);
+      }
+    }
+
+  }
 }
 
-function draw() {
+function keyPressed() {
+  if (keyCode == LEFT_ARROW) {
+    x_left = round(imgWidth/2) - round(squareSize/2);
+    y_top = round(imgHeight/2) - round(squareSize/2);
+  }
+}
 
+function mouseReleased() {
+  firstClick = true;
 }
